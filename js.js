80c95b901f215d40c83395522799643c65471820
Patrik Melis <eaapame@students.eaaa.dk>
2019-05-25 19:47:35 +0200

    Big update

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($){
 
     $('.seat').on('click',function(){
-
         if
             ($(this).attr('data-click-state') == 1) {
             $(this).attr('data-click-state', 0)
@@ -11,9 +10,83 @@ jQuery(document).ready(function($){
             $(this).attr('data-click-state', 1)
             $("*").css('background-color', 'rgba(255, 255, 255, 0)')
             $(this).css('background-color', 'green')
-
+            $(".footer").css('background-color', '#FFDB4D')
+            $(".submit").css('background-color', '#FFDB4D')
         }
+    
+    });
+    
+    if
+            ($(".seat").each(function('background-color', 'rgba(255, 255, 255, 0)')) {
+                $("seattextheading").html('- - - -')
+            })
+    
+
+    $('#bigseat1').on('click',function(){
+        $(".seattextheading").html('1');
+        
+        
+    });
+    
+    $('#bigseat2').on('click',function(){
+        $(".seattextheading").html('2');
+    });
+    
+    $('#bigseat3').on('click',function(){
+        $(".seattextheading").html('3');
+    });
+    
+    $('#bigseat4').on('click',function(){
+        $(".seattextheading").html('4');
+    });
+    
+    $('#bigseat5').on('click',function(){
+        $(".seattextheading").html('5');
+    });
+    
+    $('#bigseat6').on('click',function(){
+        $(".seattextheading").html('6');
+    });
+    
+    $('#bigseat7').on('click',function(){
+        $(".seattextheading").html('7');
+    });
+    
+    $('#bigseat8').on('click',function(){
+        $(".seattextheading").html('8');
+    });
+    
+    $('#smallseat1').on('click',function(){
+        $(".seattextheading").html('9');
+    });
+    
+    $('#smallseat2').on('click',function(){
+        $(".seattextheading").html('10');
+    });
+    
+    $('#smallseat3').on('click',function(){
+        $(".seattextheading").html('11');
+    });
+    
+    $('#smallseat4').on('click',function(){
+        $(".seattextheading").html('12');
+    });
+    
+    $('#smallseat5').on('click',function(){
+        $(".seattextheading").html('13');
+    });
+    
+    $('#smallseat6').on('click',function(){
+        $(".seattextheading").html('14');
+    });
+    
+    $('#smallseat7').on('click',function(){
+        $(".seattextheading").html('15');
+    });
+    
+    $('#smallseat8').on('click',function(){
+        $(".seattextheading").html('16');
+    });
 
 });
-
-});
+    
